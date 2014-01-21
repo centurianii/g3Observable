4856defe5ef45f98d1c1af6112cdaefbaf995047
centurianii <centurianii@yahoo.co.uk>
2014-01-21 17:46:34 +0200

    restructured

--- g3/g3Observable.js
+++ g3/g3Observable.js
@@ -1,7 +1,7 @@
-/********************************Object Observable******************************
+/********************************Object observable******************************
  * Implements the observer pattern or a publish-subscribe system based on 
  * subjects and functions registered under them.
- * Our functions signature is: function ([subject, arg1, arg2, ...]).
+ * Our functions signature is: function (Array(subject, arg1, arg2, ...)).
  * When a message is fired for a specific subject then, all registered functions 
  * are called and an array is passed to them constructed from the arguments of 
  * the notify function.
@@ -9,7 +9,7 @@
  * observers in contrast of a pull system where every observer queries the 
  * observable.
  * @module {g3}
- * @function {g3.Observable.register}
+ * @function {g3.observable.register}
  * @public
  * @param {Function} 'func' is the function that acts as the observer. A 
  * function can register under many subjects.
@@ -17,8 +17,8 @@
  * registered. If it is ommited then it defaults to 'g3'.
  * @param {Object} 'context' is the context under which the function will be 
  * executed. if it is omitted then the function is called as usual.
- * @return {Observable} It can be chained.
- * @function {g3.Observable.unregister}
+ * @return {observable} It can be chained.
+ * @function {g3.observable.unregister}
  * @public
  * @param {Function} 'func' is the function to unregister.
  * @param {String} 'subject' is the subject or event under which the function 
@@ -27,30 +27,30 @@
  * registered initialy.
  * @return {Number} It breaks the chain and returns the number of unregistered 
  * functions.
- * @function {g3.Observable.notify}
+ * @function {g3.observable.notify}
  * @public
  * @param {String} 'subject' is the subject or event of notification.
  * @return {Boolean|Number} It breaks the chain and returns the number of the 
  * functions it calls. If there are no registered functions under this subject 
  * then, it returns false.
- * @function {g3.Observable.get}
+ * @function {g3.observable.get}
  * @public
  * @param {String} 'subject' is the subject or event of notification.
  * @return {Object} It breaks the chain and returns the array of objects  
  * [{function, context}, ...] under a subject or the collection of all subjects
  * if subject is null: {length: n, subject1: [{function, context},...], ...}.
- * @function {g3.Observable.reset}
+ * @function {g3.observable.reset}
  * @public
- * @return {Observable} It can be chained. It converts the collection of all 
+ * @return {observable} It can be chained. It converts the collection of all 
  * subjects to: {length: 0}.
  *
  * @version 0.1
  * @author Scripto JS Editor by Centurian Comet.
  * @copyright MIT licence.
-*******************************************************************************/
+ ******************************************************************************/
 (function(g3, $, window, document, undefined){
    var observers = {length: 0};
-   g3.Observable = {
+   g3.observable = {
       register: function(func, subject, context){
          subject = subject || 'g3';
          if(!observers[subject])
--- tests/g3/g3Observable-Spec.js
+++ tests/g3/g3Observable-Spec.js
@@ -1,19 +1,19 @@
-describe("Object g3.Observable", function() {
+describe("Object g3.observable", function() {
    describe("Basic registration on no subject", function() {
       var o;
-      var testObservable = function( args ){
+      var observer = function( args ){
          return(args);
       };
       
       beforeEach(function() {
-         o = g3.Observable.register(testObservable);
+         o = g3.observable.register(observer);
       });
       
       afterEach(function() {
-         o.unregister(testObservable);
+         o.unregister(observer);
       });
       
-      it("should be able registration under no context (i.e. 'any') to return the Observable", function() {
+      it("should be able to register under no context (i.e. 'g3') to return the Observable", function() {
          expect(o).toBeDefined();
          expect(o).toEqual(jasmine.any(Object));
       });
@@ -24,10 +24,10 @@ describe("Object g3.Observable", function() {
          expect(o.get().length).toEqual(1);
       });
       
-      it("should be able to get an array of observers with length 1 under argument 'any'", function() {
-         expect(o.get('any')).toBeDefined();
-         expect(o.get('any')).toEqual(jasmine.any(Object)); //core js can't see arrays!
-         expect(o.get('any').length).toEqual(1);
+      it("should be able to get an array of observers with length 1 under argument 'g3'", function() {
+         expect(o.get('g3')).toBeDefined();
+         expect(o.get('g3')).toEqual(jasmine.any(Object)); //core js can't see arrays!
+         expect(o.get('g3').length).toEqual(1);
       });
       
       it("should be able to get a length of 0 after reseting the observers", function() {
@@ -36,27 +36,28 @@ describe("Object g3.Observable", function() {
          expect(o.get().length).toEqual(0);
       });
       
-      it("should not be able to get anything under argument 'any'", function() {
-         var tmp = o.get('any');
+      it("should not be able to get anything under argument 'g3'", function() {
+         o.reset();
+         var tmp = o.get('g3');
          expect(tmp).not.toBeDefined();
       });
    });
    
    describe("Basic registration on subject", function() {
       var o;
-      var testObservable = function( args ){
+      var observer = function( args ){
          return(args);
       };
       
       beforeEach(function() {
-         o = g3.Observable.register(testObservable, 'example1');
+         o = g3.observable.register(observer, 'example1');
       });
       
       afterEach(function() {
-         o.unregister(testObservable, 'example1');
+         o.unregister(observer, 'example1');
       });
       
-      it("should be able registration under context 'example1' to return the Observable", function() {
+      it("should be able to register under context 'example1' to return the Observable", function() {
          expect(o).toBeDefined();
          expect(o).toEqual(o);
       });
@@ -80,6 +81,7 @@ describe("Object g3.Observable", function() {
       });
       
       it("should not be able to get anything under argument 'example1'", function() {
+         o.reset();
          var tmp = o.get('example1');
          expect(tmp).not.toBeDefined();
       });
@@ -87,18 +89,18 @@ describe("Object g3.Observable", function() {
    
    describe("Multiple registration of the same function on a given subject and no subject", function() {
       var o, b;
-      var testObservable = function( args ){
+      var observer = function( args ){
          return(args);
       };
       
       beforeEach(function() {
-         o = g3.Observable.register(testObservable);
-         b = g3.Observable.register(testObservable, 'example1');
+         o = g3.observable.register(observer);
+         b = g3.observable.register(observer, 'example1');
       });
       
       afterEach(function() {
-         o = g3.Observable.unregister(testObservable);
-         b = g3.Observable.unregister(testObservable, 'example1');
+         o = g3.observable.unregister(observer);
+         b = g3.observable.unregister(observer, 'example1');
       });
 
       it("both registrations should return the same observable", function() {
@@ -111,10 +113,10 @@ describe("Object g3.Observable", function() {
          expect(o.get().length).toEqual(2);
       });
       
-      it("should be able to get an array of observers with length 1 under argument 'any'", function() {
-         expect(o.get('any')).toBeDefined();
-         expect(o.get('any')).toEqual(jasmine.any(Object)); //core js can't see arrays!
-         expect(o.get('any').length).toEqual(1);
+      it("should be able to get an array of observers with length 1 under argument 'g3'", function() {
+         expect(o.get('g3')).toBeDefined();
+         expect(o.get('g3')).toEqual(jasmine.any(Object)); //core js can't see arrays!
+         expect(o.get('g3').length).toEqual(1);
       });
 
       it("should be able to get an array of observers with length 1 under argument 'example1'", function() {
@@ -129,33 +131,35 @@ describe("Object g3.Observable", function() {
          expect(o.get().length).toEqual(0);
       });
       
-      it("should not be able to get anything under argument 'any'", function() {
-         var tmp = o.get('any');
+      it("should not be able to get anything under argument 'g3'", function() {
+         o.reset();
+         var tmp = o.get('g3');
          expect(tmp).not.toBeDefined();
       });
       
       it("should not be able to get anything under argument 'example1'", function() {
-         var tmp = o.get('any');
+         o.reset();
+         var tmp = o.get('example1');
          expect(tmp).not.toBeDefined();
       });
    });
    
    describe("Multiple registration of different functions on a given subject", function() {
-      var o = g3.Observable;
-      var testObservable1 = function( args ){
+      var o = g3.observable;
+      var observer1 = function( args ){
          console.log(args);
       };
-      var testObservable2 = function( args ){
+      var observer2 = function( args ){
          console.log(args);
       };
       
       beforeEach(function() {
-         o.register(testObservable1, 'example1').register(testObservable2, 'example1');
+         o.register(observer1, 'example1').register(observer2, 'example1');
       });
       
       afterEach(function() {
-         o.unregister(testObservable1, 'example1');
-         o.unregister(testObservable2, 'example1');
+         o.unregister(observer1, 'example1');
+         o.unregister(observer2, 'example1');
       });
 
       it("should be able to get a hash of subjects with length 2 under no argument", function() {
@@ -178,21 +182,21 @@ describe("Object g3.Observable", function() {
    });
    
    describe("Notifies on multiple registrations on a given subject", function() {
-      var o = g3.Observable;
-      var testObservable1 = function( args ){
+      var o = g3.observable;
+      var observer1 = function( args ){
          console.log(args);
       };
-      var testObservable2 = function( args ){
+      var observer2 = function( args ){
          console.log(args);
       };
       
       beforeEach(function() {
-         o.register(testObservable1, 'example1').register(testObservable2, 'example1');
+         o.register(observer1, 'example1').register(observer2, 'example1');
       });
       
       afterEach(function() {
-         o.unregister(testObservable1, 'example1');
-         o.unregister(testObservable2, 'example1');
+         o.unregister(observer1, 'example1');
+         o.unregister(observer2, 'example1');
       });
 
       it("should be able to get a hash of subjects with length 2 under no argument", function() {
@@ -202,17 +206,18 @@ describe("Object g3.Observable", function() {
       });
       
       it("should be able to get 2 notifies from the observers", function() {
-         expect(o.notify('hello world!')).toEqual(2);
+         expect(o.notify('example1', 'hello world!')).toEqual(2);
       });
       
       it("should be able to get 1 notify after unregistering 1 observer", function() {
-         o.unregister(testObservable1, 'example1');
-         expect(o.notify('hello world!')).toEqual(1);
+         o.unregister(observer1, 'example1');
+         expect(o.notify('example1', 'hello world!')).toEqual(1);
       });
       
       it("should be able to get false after unregistering the last observer", function() {
-         o.unregister(testObservable1, 'example1');
-         expect(o.notify(['test','a','b','c'])).toBe(false);
+         o.unregister(observer1, 'example1');
+         o.unregister(observer2, 'example1');
+         expect(o.notify('example1', ['test','a','b','c'])).toBe(false);
       });
    });
 });
